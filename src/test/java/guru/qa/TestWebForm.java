ae8e61ae57c3cc36a94c9cd40597fbe4d147a404
Yury.Fedoseev <Yri95@inbox.ru>
2023-02-21 17:11:31 +0300

    Full test

--- src/test/java/guru/qa/TestWebForm.java
+++ src/test/java/guru/qa/TestWebForm.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -20,7 +22,6 @@ public class TestWebForm {
         $("#userNumber").setValue("9876543211").click();
         $("#dateOfBirthInput").doubleClick().pressEnter();
         //$("# css-2b097c-container").setValue("Мира");
-        //$("#Current Address").setValue("Мира и гагарина");
         //$("#dateOfBirthInput").contextClick().clear();
         //$("#dateOfBirthInput").doubleClick().clear();
         //$("#dateOfBirthInput").setValue("18 Feb 2022").pressTab();
@@ -33,6 +34,56 @@ public class TestWebForm {
         $x("//div[@class='table-responsive']").shouldHave(Condition.text("Female"));
         $x("//div[@class='table-responsive']").shouldHave(Condition.text("9876543211"));
 
+
+    }
+
+    @Test
+    void FullTest() {
+        Configuration.holdBrowserOpen = true;
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Иван").pressTab();
+        $("#lastName").setValue("Фамилию").pressTab();
+        $("#userEmail").setValue("mail@muil.ru").pressTab();
+        $(byTagAndText("Label", "Female")).click();
+        $("#currentAddress").setValue("Мира и Гагарина").pressTab().pressEnter();
+        $("#subjectsInput").setValue("Chemistry").pressEnter();
+        $(byTagAndText("Label", "Music")).click();
+
+        File pictures = new File("src/test/resources/skrin/379-scaled.jpg");
+        $("#uploadPicture").uploadFile(pictures);
+
+        $("#state").click();
+        $("#react-select-3-option-0").click();
+/*      $("#css-1uccc91-singleValue").setValue("Haryana").pressEnter().click();
+        $("#state").setValue("Haryana").pressEnter();
+        $("#city").setValue("Karnal").pressEnter();
+        Доделать, так не работает выбор нужного элемента*/
+
+
+        $("#city").click();
+        $("#react-select-4-option-0").click();
+
+        $("#userNumber").setValue("9876543211").pressEnter();
+
+//Выбор даты от учителя
+       /* $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $("option[value='3']").click();
+        $(".react-datepicker__year-select").click();
+        $("option[value='2005']").click();
+        $("div[aria-label='Choose Sunday, April 3rd, 2005']").click();*/
+
+        //Проверка финальной формы
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Иван"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Фамилию"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("mail@muil.ru"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Female"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("9876543211"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Chemistry"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Music"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("379-scaled.jpg"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Мира и Гагарина"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("NCR Delhi"));
 
     }
 }
